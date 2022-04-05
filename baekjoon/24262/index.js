31ff782f06aa234718436f59969be9adcb90ab71
eungikim <eungikim.me@gmail.com>
2022-04-05 17:37:45 +0900

    Solve baekjoon 24262

---
+++ baekjoon/24262/index.js
@@ -0,0 +1,35 @@
+// https://www.acmicpc.net/problem/24262
+// TAG 구현, 시뮬레이션
+// solved.ac Bronze 5
+// 첫째 줄에 입력의 크기 n(1 ≤ n ≤ 500,000)이 주어진다.
+
+// const fs = require("fs");
+// const inputPath =
+//   process.platform === "darwin" ? __dirname + "/data.txt" : "/dev/stdin";
+// const input = fs.readFileSync(inputPath).toString();
+
+console.log(1);
+console.log(0);
+
+// const inputNumber = Number(input);
+// solve(inputNumber);
+
+// function solve(inputSize) {
+//   let size = inputSize;
+//   let executionTime = 0;
+//   while (size > 0) {
+//     size = parseInt(size / 2);
+//     executionTime++;
+//   }
+
+//   console.log(executionTime);
+//   console.log(getBigOTime(inputSize, executionTime));
+//   // printEx();
+// }
+
+// function getBigOTime(inputSize, executionTime) {
+//   let bigOTime = 0;
+//   if (executionTime > inputSize) {
+//     // ... Some code
+//   }
+// }
