676ca6cd535b2f98a7ac868f55771574882d0045
eungikim <eungikim.me@gmail.com>
2022-04-05 16:31:47 +0900

    Solve baekjoon 16170

---
+++ baekjoon/16170/index.js
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/16170
+// TAG 구현
+// solved.ac Bronze 5
+
+solve();
+
+function solve() {
+  const date = new Date();
+  console.log(date.getUTCFullYear());
+  console.log(date.getUTCMonth() + 1);
+  console.log(date.getUTCDate());
+}
