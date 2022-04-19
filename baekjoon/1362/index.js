0bc506a612911654e8c2bd032cd44e37898095d5
eungikim <eungikim.me@gmail.com>
2022-04-19 16:29:14 +0900

    Solve baekjoon 1362

---
+++ baekjoon/1362/index.js
@@ -0,0 +1,60 @@
+// https://www.acmicpc.net/problem/1362
+// TAG 구현
+// solved.ac Bronze 2
+// 적정 체중(o)와 실제 체중(w)가 한 줄에 입력됨으로써 시나리오가 시작됩니다(10 ≤ o, w ≤ 1000)
+// 'E' 또는 'F'로 시작하며, 공백을 두고 숫자 n (1 ≤ n ≤ 999)
+
+const fs = require("fs");
+const inputPath =
+  process.platform === "darwin" ? __dirname + "/data.txt" : "/dev/stdin";
+const scenario = fs.readFileSync(inputPath).toString().trim().split("\n");
+
+const PetStatus = {
+  HAPPY: ":-)",
+  SAD: ":-(",
+  DIED: "RIP",
+};
+
+solve(scenario);
+
+function solve(petScenario) {
+  let scenarioNumber = 0;
+  while (true) {
+    let [appropriateWeight, currentWeight] = petScenario
+      .shift()
+      .split(" ")
+      .map(Number);
+
+    if (appropriateWeight === 0) break;
+    scenarioNumber++;
+    let isDead = false;
+
+    while (true) {
+      const [cmd, value] = petScenario.shift().split(" ");
+      if (cmd === "E" && !isDead) {
+        currentWeight -= Number(value);
+        if (currentWeight <= 0) {
+          isDead = true;
+        }
+      } else if (cmd === "F" && !isDead) {
+        currentWeight += Number(value);
+      } else if (cmd === "#") {
+        printPetStatus(scenarioNumber, appropriateWeight, currentWeight);
+        break;
+      }
+    }
+  }
+}
+
+function printPetStatus(scenarioNumber, appropriateWeight, currentWeight) {
+  if (currentWeight <= 0) {
+    console.log(`${scenarioNumber} ${PetStatus.DIED}`);
+  } else if (
+    appropriateWeight / 2 < currentWeight &&
+    currentWeight < appropriateWeight * 2
+  ) {
+    console.log(`${scenarioNumber} ${PetStatus.HAPPY}`);
+  } else {
+    console.log(`${scenarioNumber} ${PetStatus.SAD}`);
+  }
+}
